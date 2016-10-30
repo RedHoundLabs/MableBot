035630e67b2a9dbcc3d33ba628f611596056a40d
kiricon <kiricon@live.com>
2016-10-30 14:56:02 -0600

    Added some comments and cleaned up the code

--- System/keyboard.py
+++ System/keyboard.py
@@ -1,8 +1,14 @@
 from pykeyboard import PyKeyboard
 keyboard = PyKeyboard()
 
+''' Press key on the keyboard '''
 def press(character):
-    keyboard.press_key(character)
+    keyboard.tap_key(character)
 
+''' Type a string '''
 def type(string):
     keyboard.type_string(string)
+
+''' Press Multiple Keys '''
+def pressMultiple(arr):
+    keyboard.press_keys(arr)
