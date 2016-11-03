b5f403828a91d0bd271622791f39f47ccdad45da
kiricon <kiricon@live.com>
2016-11-02 18:24:37 -0600

    Reading image grabs in to opencv now

--- System/readimage.py
+++ System/readimage.py
@@ -1,7 +1,19 @@
 import cv2
 from PIL import ImageGrab
 from PIL import Image
+import numpy
+from matplotlib import pyplot as plt
 
-test = ImageGrab.grab()
-Image._show(test)
+pil_image = ImageGrab.grab()
+img = cv2.cvtColor(numpy.array(pil_image), cv2.COLOR_RGB2BGR)
 
+'''img = cv2.imread(opencvImage,0)'''
+edges = cv2.Canny(img,100,200)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+plt.show()
+
+'''Image._show(test)'''
