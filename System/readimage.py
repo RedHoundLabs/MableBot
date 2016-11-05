f01e1c6cdb89be20ab0aa1a40f2da536ebceeefc
kiricon <kiricon@live.com>
2016-11-04 21:55:32 -0600

    Screen selection is working well. Just need to store the value now for bbox

--- System/readimage.py
+++ System/readimage.py
@@ -1,41 +1,62 @@
 import cv2
 from PIL import ImageGrab
 from PIL import Image
-import numpy as np
+import numpy
 from matplotlib import pyplot as plt
 
-'''Read image from screen and convert it to opencv'''
+# initialize the list of reference points and boolean indicating
+# whether cropping is being performed or not
+refPt = []
+cropping = False
+
+def click_and_crop(event, x, y, flags, param):
+	# grab references to the global variables
+	global refPt, cropping
+
+	# if the left mouse button was clicked, record the starting
+	# (x, y) coordinates and indicate that cropping is being
+	# performed
+	if event == cv2.EVENT_LBUTTONDOWN:
+		refPt = [(x, y)]
+		cropping = True
+
+	# check to see if the left mouse button was released
+	elif event == cv2.EVENT_LBUTTONUP:
+		# record the ending (x, y) coordinates and indicate that
+		# the cropping operation is finished
+		refPt.append((x, y))
+		cropping = False
+
+		# draw a rectangle around the region of interest
+		cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
+		cv2.imshow("image", image)
+
+
+# load the image, clone it, and setup the mouse callback function
 pil_image = ImageGrab.grab()
-img = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
+image = cv2.cvtColor(numpy.array(pil_image), cv2.COLOR_RGB2BGR)
+clone = image.copy()
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", click_and_crop)
 
+# keep looping until the 'q' key is pressed
+while True:
+	# display the image and wait for a keypress
+	cv2.imshow("image", image)
+	key = cv2.waitKey(1) & 0xFF
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = cv2.bilateralFilter(gray, 11, 17, 17)
-edged = cv2.Canny(gray, 30, 200)
+	# if the 'c' key is pressed, break from the loop
+	if key == ord("c"):
+		break
 
+# if there are two reference points, then crop the region of interest
+# from teh image and display it
+cv2.destroyWindow("image")
+if len(refPt) == 2:
+	roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+	cv2.imshow("ROI", roi)
 
-# find contours in the edged image, keep only the largest
-# ones, and initialize our screen contour
-(_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
-screenCnt = None
+	cv2.waitKey(0)
 
-for c in cnts:
-	# approximate the contour
-	peri = cv2.arcLength(c, True)
-	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-
-	# if our approximated contour has four points, then
-	# we can assume that we have found our screen
-	if len(approx) == 4:
-		screenCnt = approx
-
-
-cv2.drawContours(img, [screenCnt], -1, (0, 255, 0), 3)
-
-plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.show()
-
-'''cv2.imwrite('houghlines3.jpg',img)'''
-'''Image._show(test)'''
+# close all open windows
+cv2.destroyAllWindows()
